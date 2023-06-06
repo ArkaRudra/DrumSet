d638579b6533195cea6b41775bea485ee06dd867
ArkaRudra <codev.arko1@gmail.com>
2023-06-06 17:07:48 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,102 @@
+
+//Detecting Button Press
+    var All_Drums = document.querySelectorAll(".drum").length ;
+
+
+    for(let i =0;i<All_Drums; i++) {
+      document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        var buttonInnerHTML = this.innerHTML;
+
+         makeSound(buttonInnerHTML);
+
+    });
+
+
+
+
+  //Detecting Keyboard Press
+      document.addEventListener("keydown", function(event){
+        makeSound(event.key);
+
+      });
+
+  
+      function makeSound(key) {
+        
+      switch(key) {
+     
+        case "w":
+
+         var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+
+        break;
+
+       case "a":
+
+          var tom2 = new Audio("sounds/tom-2.mp3")
+          tom2.play()
+       
+        break;
+
+
+        case "s":
+
+          var tom4 = new Audio("sounds/tom-4.mp3")
+          tom4.play()
+        
+        break;
+
+
+
+        case "d":
+        
+          var tom1 = new Audio("sounds/tom-1.mp3")
+          tom1.play()
+
+        break;
+
+
+
+        case "j":
+          
+          var kick = new Audio("sounds/kick-bass.mp3")
+          kick.play()
+
+        break;
+
+        case "k":
+          
+        var tom3 = new Audio("sounds/tom-3.mp3")
+          tom3.play()
+        
+        break;
+
+
+        case "l":
+
+          var crash = new Audio("sounds/crash.mp3")
+          crash.play()
+        
+        break;
+
+        default:console.log("Wrong button. You have pressed:" + buttonInnerHTML)
+        
+     }    
+  }
+  
+}
+
+
+
+
+// var audio = new Audio("sounds/crash.mp3")
+// audio.play()
+
+
+
+
+// document.querySelectorAll("button")[i].addEventListener("click", function(){
+//     alert("I got clicked!")
+// })
